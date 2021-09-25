058b525cd1affbdbe208d36dec1d87e5a0ba6527
Boluwatife <jaycewap@gmail.com>
2021-09-26 00:33:49 +0100

    :star: Test passed.

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,59 +1,202 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-unit-jest" target="_blank" rel="noopener">unit-jest</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
-  </div>
+  <section id="app" class="container">
+    <form class="form" @submit.prevent="validateForm()">
+      <h1 class="form__heading">Contact Us!</h1>
+      <div id="response" :class="errorClass"></div>
+      <label for="name" :class="`input-group ${user.name.length !== 0 && (user.name.length < 5 || user.name.length > 50 ? 'input-group--danger' : '')}`">
+        <span class="input-group__label">name</span>
+        <input id="name" type="text" v-model="user.name" class="input-group__input" required/>
+      </label>
+      <label for="email" class="input-group">
+        <div id="err"></div>
+        <span class="input-group__label">email</span>
+        <input id="email" type="email" v-model="user.email" class="input-group__input" required/>
+      </label>
+      <label for="subject" class="input-group">
+        <span class="input-group__label">subject</span>
+        <input id="subject" type="text" v-model="user.subject" :class="`input-group__input ${user.subject.length > 100 ? 'input-group--danger' : ''}`" />
+      </label>
+      <label for="message" class="input-group">
+        <span :class="`input-group__label ${user.message.length > 500 ? 'input-group--danger':''}`">message</span>
+        <textarea id="message" type="text" v-model="user.message" class="input-group__input" required></textarea>
+      </label>
+      <button class="btn" id="btn" :disabled="user.message.length == 0">send</button>
+    </form>
+  </section>
 </template>
 
 <script>
+import axios from 'axios'
+
 export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
+  name: 'App',
+  data() {
+    return {
+      user: {
+        name: '',
+        email: '',
+        subject: '',
+        message: ''
+      },
+      error: false,
+      errorClass: ''
+    }
+  },
+  methods: {
+    validateForm() {
+      this.error = true
+      let response = document.getElementById('response')
+      let btn = document.getElementById('btn')
+      let user = this.user
+
+      btn.disabled = true //disable the submit button
+      btn.innerText = 'Please wait...'
+
+      if((/^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,})/).test(this.user.email)) { // first validate e-mail
+        if(user.name.length > 5 && user.name.length <= 50) {
+        // Send data
+          if(user.message.length > 1 && user.message.length <=100 ){
+            axios.post('https://5d9f7fe94d823c0014dd323d.mockapi.io/api/message', this.user, {
+            headers: {
+              'Access-Control-Allow-Origin': '*'
+            }
+          }).then(result => {
+            this.errorClass = 'alert alert--success'
+            response.innerText =`Thank you ${this.user.name}. Your message has been recieved.`
+            console.log(result)
+          }).catch(error => {
+            response.innerText = `Oops, looks like we ran into an error while recieving your message. Please try again.`
+            console.log(error)
+          })
+          } else {
+            this.errorClass = 'alert alert--danger'
+            response.innerText = 'Your message is more than 100 characters.'
+          }
+        } else {
+          this.errorClass = 'alert alert--danger'
+          response.innerText = 'Your name is more than 50 characters or less than 5 characters.'
+        }
+      } else {
+        this.error = true
+        this.errorClass = 'alert alert--danger'
+        response.innerText = `${this.user.email} is not a valid email address`
+      }
+    }
   }
 }
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
+<style>
+*,
+*::after,
+*::before {
   padding: 0;
+  margin: 0;
+  box-sizing: border-box;
 }
-li {
-  display: inline-block;
-  margin: 0 10px;
+
+html {font-size: 62.5%;}
+body {font-size: 2rem;}
+
+:root {--base: rgb(0, 224, 161);}
+.container {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  height: 100vh;
+  background-color: white;
 }
-a {
-  color: #42b983;
+
+.form {
+  border: .2rem solid var(--base);
+  background-color: transparent;
+  display: inline-flex;
+  flex-direction: column;
+  justify-content: space-between;
+  align-items: flex-start;
+  border-radius: 2rem;
+  padding: 5rem;
+}
+
+.input-group {
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  margin-block-start: 1.5rem;
+  border-bottom: .2rem solid rgba(0, 224, 161, .5);
+  transition: border 300ms ease-in-out;
+}
+
+.input-group:focus-within {border-color: rgb(0, 224, 161);}
+
+.input-group--danger {
+  border-color: tomato;
+}
+
+.input-group__label {
+  text-transform: capitalize;
+  font-size: 1.8rem;
+  opacity: .7;
+}
+
+.input-group__input {
+  border: none;
+  outline: none;
+  background: transparent;
+  padding: .5rem;
+  font-size: 2rem;
+}
+
+.btn {
+  display: flex;
+  margin-block-start: 3rem;
+  padding: 1.5rem 5rem;
+  outline: none;
+  border: none;
+  border-radius: 3rem;
+  background: var(--base);
+  border: .3rem solid rgba(255,255,255,.7);
+  color: white;
+  cursor: pointer;
+  font-weight: 600;
+  text-transform: uppercase;
+  letter-spacing: .2rem;
+  transition: border 300ms ease-in-out;
+}
+
+.btn:hover {
+  border-color: rgba(0,0,0,.3);
+}
+
+.btn[disabled] {
+  cursor: not-allowed;
+  filter: grayscale(100%);
+  }
+
+.alert {
+  font-size: 1.5rem;
+  display: block;
+  padding: 1rem;
+  margin-block-end: 1rem;
+  border-left: .5rem solid transparent;
+  color: transparent;
+  transition: 300ms ease-in-out;
+  text-align: left;
+}
+
+.alert--error {
+  border-color: rgb(255, 99, 71);
+  background: rgba(255, 99, 71, .5);
+  color: black;
+}
+
+.alert--success {
+  border-color: rgb(0, 224, 161);
+  background: rgba(0, 224, 161, .5);
+  color: black;
 }
 </style>
--- tests/unit/example.spec.js
+++ tests/unit/example.spec.js
@@ -1,12 +1,21 @@
-import { shallowMount } from '@vue/test-utils'
+import flushPromises from "flush-promises"
+import Vue from 'vue'
+import { mount } from '@vue/test-utils'
 import HelloWorld from '@/components/HelloWorld.vue'
 
-describe('HelloWorld.vue', () => {
-  it('renders props.msg when passed', () => {
-    const msg = 'new message'
-    const wrapper = shallowMount(HelloWorld, {
-      propsData: { msg }
-    })
-    expect(wrapper.text()).toMatch(msg)
+jest.mock('axios')
+
+it('Sends a message containing the user.name when success', async() => {
+  const user = {name: 'John Doe', email: 'johndoe@company.agency', message: 'Test message from Jest'}
+  const wrapper = mount(HelloWorld)
+
+  await wrapper.find('#name').setValue(user.name)
+  await wrapper.find('#email').setValue(user.email)
+  await wrapper.find('#message').setValue(user.message)
+  await wrapper.find('form').trigger("submit.prevent")
+
+  wrapper.vm.$nextTick().then( ()=> {
+    expect.assertions(1)
+    expect(wrapper.find('#response').text()).toContain(`Thank you ${user.name}. Your message has been recieved.`)
   })
 })
